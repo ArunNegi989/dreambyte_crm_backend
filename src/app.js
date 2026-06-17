641d520e42f024e67117a219f4a1566855b91465
Arun Negi <Arun-Negi989>
2026-06-17 09:41:37 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Health Check
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API is running...",
+  });
+});
+
+// API Routes
+app.use("/api", routes);
+
+module.exports = app;
