c76a338faaa5beccff53ee40e5cfe36c7fe73cf8
Arun Negi <Arun-Negi989>
2026-06-29 10:56:13 +0530

    testing

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 5000;
   try {
     await connectDB();
 
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
+    app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
+});
   } catch (error) {
     console.error("Server startup failed:", error);
     process.exit(1);
